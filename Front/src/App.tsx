3892d74202c8e77e58928c1fe8b765054a5e890a
kwonja <kownja87@gmail.com>
2024-09-12 11:34:17 +0900

    style: 폰트 추가

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,21 +1,10 @@
+
+import './App.css';
 function App() {
   return (
     <>
-      <div>폰트 테스트</div>
-      <div>폰트 테스트</div>
-
-
-
-
-
-
-   
-
-
-      
-
-      
-      프리티어 허스키 테스트
+      <div className='Pretendard'>폰트 테스트</div>
+      <div className="old">폰트 테스트</div>
     </>
   );
 }
--- Front/src/main.tsx
+++ Front/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 
+
+import "../src/assets/font/_font.scss"
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
