c61259e0c205af0107789f69baa2c2cdad393ce8
kwonja <kownja87@gmail.com>
2024-09-13 10:17:16 +0900

    test: app.tsx 프리티어 테스트

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
     <>
       <div className="Pretendard">폰트 테스트</div>
       <div className="old">폰트 테스트</div>
-      프리티어 바로 적용하고 커밋하기 테스트 123123123123
+      진짜 마지막 프리티어 바로 적용하고 커밋하기 테스트 123123123123
     </>
   );
 }
