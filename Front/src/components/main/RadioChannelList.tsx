1a383b10cd1ef56aeaf4e221a3c21fc26044b43c
이윤주 <yunj33@naver.com>
2024-09-17 23:35:58 +0900

    feat: 메인페이지 검색바 추가, react-slick 라이브러리 추가

--- Front/src/components/main/RadioChannelList.tsx
+++ Front/src/components/main/RadioChannelList.tsx
@@ -1,8 +1,21 @@
+import styles from './RadioChannelList.module.scss';
+
 const RadioChannelList = () => {
+  const channels = ['Channel 1', 'Channel 2', 'Channel 3'];
+
   return (
-    <>
-      <h1>라디오 채널</h1>
-    </>
+    <div className={styles.radioList}>
+      <div>라디오 채널</div>
+      <div>관심분야의 논문 라디오를 들어보세요</div>
+      {channels.map((channel, index) => (
+        <div
+          key={index}
+          className={styles.channelItem}
+        >
+          {channel}
+        </div>
+      ))}
+    </div>
   );
 };
 
