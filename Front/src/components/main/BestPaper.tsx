7bf0ba795014e46a070c12131984f716527da044
이윤주 <yunj333@gmail.com>
2024-09-17 23:35:58 +0900

    feat: 메인페이지 검색바 추가, react-slick 라이브러리 추가

--- Front/src/components/main/BestPaper.tsx
+++ Front/src/components/main/BestPaper.tsx
@@ -1,5 +1,12 @@
+import styles from './BestPaper.module.scss';
+
 const BestPaper = () => {
-  return <></>;
+  return (
+    <div className={styles.bestPaper}>
+      <h3>Best Paper of the Month</h3>
+      <p>This is a summary of the best paper.</p>
+    </div>
+  );
 };
 
 export default BestPaper;
--- Front/src/components/main/SearchBar.tsx
+++ Front/src/components/main/SearchBar.tsx
@@ -1,9 +1,30 @@
-const SearchBar = () => {
+import React, { useState } from 'react';
+import styles from './SearchBar.module.scss';
+import searchIcon from '../../assets/images/search.svg'; // SVG 아이콘 불러오기
+
+const SearchBar: React.FC = () => {
+  const [value, setValue] = useState<string>('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
   return (
-    <>
-      {' '}
-      <h1>서치 바</h1>
-    </>
+    <div className={styles.searchBar}>
+      <img
+        src={searchIcon}
+        alt="검색 아이콘"
+        className="icon"
+      />
+      <input
+        type="text"
+        value={value}
+        onChange={handleChange}
+        placeholder="제목, 저자, 키워드로 논문을 검색하세요."
+        className={styles.input}
+      />
+      <button className={styles.button}>검색하기</button>
+    </div>
   );
 };
 
