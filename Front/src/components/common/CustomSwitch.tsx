9ad8fab18691d7f7f4ea351af38313307e80926f
kwonja <kownja87@gmail.com>
2024-09-17 23:17:54 +0900

    feat: 스위치 버튼 반응형 구현

--- Front/src/components/common/CustomSwitch.tsx
+++ Front/src/components/common/CustomSwitch.tsx
@@ -12,13 +12,15 @@ const CustomSwitch = () => {
       onChange={setEnabled}
       className={`group relative flex items-center justify-between h-10 w-20 cursor-pointer rounded-full p-1 transition-colors duration-200 ease-in-out focus:outline-none ${
         enabled ? 'bg-dark-toggle-background' : 'bg-light-toggle-background'
-      }`}
+      }
+      mobile:h-8 mobile:w-14
+      `}
     >
       <span
         aria-hidden="true"
         className={`inline-block h-8 w-8 rounded-full bg-cover transition-transform duration-200 ease-in-out ${
           enabled ? 'translate-x-10' : 'translate-x-0'
-        }`}
+        } mobile:h-6 mobile:w-6 ${enabled ? 'mobile:translate-x-6' : 'mobile:translate-x-0'}`}
         style={{
           backgroundImage: `url(${enabled ? Dark : Light})`,
           backgroundSize: 'contain',
--- Front/tailwind.config.js
+++ Front/tailwind.config.js
@@ -1,8 +1,13 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: { max: '360px' }, // 360px 이하에서 적용
+      },
+    },
     colors: {
       'light-toggle-background': '#E9E9E9',
       'dark-toggle-background': '#626262',
