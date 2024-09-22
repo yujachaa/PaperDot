e7238bd4179efcf375e6a5b5398eea0c15e24a24
kwonja <kownja87@gmail.com>
2024-09-23 01:17:31 +0900

    feat: 채팅입력 다크모드 추가 및 메세지 다크모드 수정

--- Front/src/components/chat/SendInput.tsx
+++ Front/src/components/chat/SendInput.tsx
@@ -1,16 +1,18 @@
 import styles from './SendInput.module.scss';
 import sendImage from '../../assets/images/입력.svg';
+import useTheme from '../../zustand/theme';
 type InputProps = {
   className?: string;
 };
 
 const SendInput = ({ className }: InputProps) => {
+  const isDarkMode = useTheme((state) => state.isDarkMode);
   return (
-    <div className={`${styles.box} ${className}`}>
+    <div className={`${styles.box} ${className} ${isDarkMode ? `${styles.dark}` : ''}`}>
       <input
         type="text"
         placeholder="댓글 입력..."
-        className={`${styles.input}`}
+        className={`${styles.input} ${isDarkMode ? `${styles.dark}` : ''}`}
       />
       <img
         src={sendImage}
