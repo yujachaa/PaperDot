99f0ef9c2e369ca57cfa1b14a6bea345a7f14da4
장현수 <wkdalsrud113@naver.com>
2024-09-23 10:41:27 +0900

    design: 마이페이지 비밀번호

--- Front/src/components/mypage/ChangePassword.tsx
+++ Front/src/components/mypage/ChangePassword.tsx
@@ -1,8 +1,32 @@
+import styles from './ChangePassword.module.scss';
+
 const ChangePassword: React.FC = () => {
   return (
-    <>
-      <p>비밀번호 변경</p>
-    </>
+    <div className={styles.ChangePassword}>
+      <form className={styles.changeform}>
+        <div className={styles.formGroup}>
+          <label>현재 비밀번호</label>
+          <input type="password" />
+        </div>
+
+        <div className={styles.formGroup}>
+          <label>새 비밀번호 입력</label>
+          <input type="password" />
+        </div>
+
+        <div className={styles.formGroup}>
+          <label>새 비밀번호 확인</label>
+          <input type="password" />
+        </div>
+
+        <button
+          type="submit"
+          className={styles.submitButton}
+        >
+          수정
+        </button>
+      </form>
+    </div>
   );
 };
 
