1d9ff9b8dad9ba2e0ee6533141f2578c3f201ff8
kwonja <kownja87@gmail.com>
2024-09-25 11:06:16 +0900

    feat: test 패키지 설치 및 채팅테스트 작성

--- Front/src/first.test.js
+++
@@ -1,5 +0,0 @@
-import { expect, test } from 'vitest';
-
-test('1 is 1', () => {
-  expect(1).not.toBe(1);
-});
---
+++ Front/src/interface/chat.ts
@@ -0,0 +1,6 @@
+export interface GroupMessage {
+  dmMessageId: number;
+  message: string;
+  Writernickname: string;
+  WriterId: number;
+}
