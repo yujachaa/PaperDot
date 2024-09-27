8ba0c7d60698ec70ba84252afc65f6074778073e
kwonja <kownja87@gmail.com>
2024-09-27 09:44:09 +0900

    fix: eslint 경로 재설정 및 파서 추가

--- Front/eslint.config.js
+++ Front/eslint.config.js
@@ -1,31 +1,30 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import prettier from "eslint-config-prettier";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-config-prettier';
+import typescriptParser from '@typescript-eslint/parser';
 const config = [
   js.configs.recommended,
   {
     // ESLint 기본 설정
+    files: ['src/**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: typescriptParser,
     },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
     rules: {
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-      "no-var": "error",
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'no-var': 'error',
       // "no-console": ["warn", { allow: ["warn", "error", "info"] }],
-      eqeqeq: "error",
-      "dot-notation": "error",
-      "no-unused-vars": "error",
+      eqeqeq: 'error',
+      'dot-notation': 'error',
+      'no-unused-vars': 'error',
     },
   },
   // Prettier 설정을 추가합니다.
