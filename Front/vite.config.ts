b0058d494386800105357cc0535a4ef20d88ec7a
kwonja <kownja87@gmail.com>
2024-09-27 16:00:50 +0900

    fix: vite image-url 쌍따옴표 추가

--- Front/vite.config.ts
+++ Front/vite.config.ts
@@ -5,5 +5,8 @@ import tsconfigPaths from 'vite-plugin-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    assetsInlineLimit: 20000, // 20KB로 설정
+  },
   plugins: [react(), svgr(), tsconfigPaths()],
 });
