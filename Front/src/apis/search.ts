26d94cc786ff56bdf87373fdac6c143f6ad16698
이윤주 <yunj33@naver.com>
2024-10-02 09:16:27 +0900

    refactor: searchApi -> core.ts 수정

--- Front/src/apis/search.ts
+++ Front/src/apis/search.ts
@@ -1,23 +1,4 @@
-import axios from 'axios';
-
-// 환경 변수에서 사용자명과 비밀번호 가져오기
-const username = import.meta.env.VITE_ELASTIC_USERNAME;
-const password = import.meta.env.VITE_ELASTIC_PW;
-
-// Elasticsearch API 기본 URL
-const ELASTIC_URL = 'http://j11b208a.p.ssafy.io:9200';
-
-// axios 인스턴스 생성 (기본 URL 설정)
-const searchApi = axios.create({
-  baseURL: ELASTIC_URL,
-  auth: {
-    username: username,
-    password: password,
-  },
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+import { searchApi } from './core';
 
 // 검색 API 호출 함수
 export const searchTitle = async (queryTerm: string) => {
