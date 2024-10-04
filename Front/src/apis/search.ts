dc684f36bb8a48e339e498da31c3632522b321a4
이윤주 <yunj333@gmail.com>
2024-10-04 14:29:38 +0900

    feat: 논문 상세 interface 수정, 논문검색 api 추가

--- Front/src/apis/search.ts
+++ Front/src/apis/search.ts
@@ -1,6 +1,6 @@
-import { searchApi } from './core';
+import { api, searchApi } from './core';
 
-// 검색 API 호출 함수
+// 검색창 자동완성 api
 export const searchTitle = async (queryTerm: string) => {
   const requestBody = {
     query: {
@@ -15,7 +15,29 @@ export const searchTitle = async (queryTerm: string) => {
     const response = await searchApi.post('/papers/_search', requestBody);
     return response.data;
   } catch (error) {
-    console.error('Error searching papers:', error);
+    console.error('검색 자동완성 에러!:', error);
+    throw error;
+  }
+};
+
+// 검색결과페이지 첫번째 api
+export const getSearchResult = async (queryTerm: string) => {
+  try {
+    const response = await api.get(`/api/papers/search?keyword=${queryTerm}`);
+    return response.data;
+  } catch (error) {
+    console.error('첫번째 검색페이지 에러!:', error);
+    throw error;
+  }
+};
+
+//검색결과 페이지네이션 api
+export const getSearchPage = async (queryTerm: string, pageNo: number) => {
+  try {
+    const response = await api.get(`/api/papers/search?keyword=${queryTerm}&pageNo=${pageNo}`);
+    return response.data;
+  } catch (error) {
+    console.error('검색 페이지네이션 에러!:', error);
     throw error;
   }
 };
