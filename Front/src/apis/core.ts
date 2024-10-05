0f97f2aca8a4d7c25cd434d808fe59f5d8a5d12e
장현수 <wkdalsrud113@naver.com>
2024-10-05 20:34:58 +0900

    feature: userStar

--- Front/src/apis/core.ts
+++ Front/src/apis/core.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
-export const BASE_URL = 'http://localhost:8080';
-// export const BASE_URL = 'https://j11b208.p.ssafy.io';
+// export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'https://j11b208.p.ssafy.io';
 
 // Elasticsearch API 기본 URL
 const ELASTIC_URL = 'https://j11b208a.p.ssafy.io';
