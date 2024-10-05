95eab3fe9e649c0630b9956ceebdd91a5b84f1a9
이찬호 <leechanho96@naver.com>
2024-10-05 21:33:22 +0900

    fix: modify webConfig.java

--- Back/paperdot/src/main/java/gomgook/paperdot/config/web/WebConfig.java
+++ Back/paperdot/src/main/java/gomgook/paperdot/config/web/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:5173") // 안에 해당 주소를 넣어도 됨
+                        .allowedOriginPatterns("http://localhost:5173", "https://j11b208.p.ssafy.io") // 안에 해당 주소를 넣어도 됨
                         .allowedHeaders("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
                         .exposedHeaders("Authorization", "RefreshToken")
