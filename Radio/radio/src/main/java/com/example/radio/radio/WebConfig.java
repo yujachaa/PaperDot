43a13fbad72900a0aa48e12bebe71af585cd298b
권성민 <snna58@naver.com>
2024-10-05 21:54:22 +0900

    Update Radio/radio/src/main/java/com/example/radio/radio/WebConfig.java
    Former-commit-id: d3038a6d900208515cca23660933f3aff47f2c0f

--- Radio/radio/src/main/java/com/example/radio/radio/WebConfig.java
+++ Radio/radio/src/main/java/com/example/radio/radio/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 엔드포인트에 대해 CORS 허용
-                .allowedOrigins("http://localhost:5173", "http://localhost:5174","https://j11b208.p.ssafy.io/")
+                .allowedOrigins("http://localhost:5173", "http://localhost:5174","https://j11b208.p.ssafy.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메서드
                 .allowedHeaders("*")  // 모든 헤더 허용
                 .allowCredentials(true);  // 자격 증명 허용
