5e9c0564bffedc318531575a029bda2aec81550d
kwonja <kownja87@gmail.com>
2024-10-06 14:30:36 +0900

    feat: 라디오채널 타입 폴더이동

--- Front/src/pages/Radio.tsx
+++ Front/src/pages/Radio.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import ChatRoom from '../components/chat/ChatRoom';
 import Header from '../components/common/Header';
 import PlayList from '../components/radio/PlayList';
@@ -9,11 +9,7 @@ import Hls from 'hls.js';
 import Modal from '../components/radio/Modal';
 const Radio = () => {
   const [isModalOpen, setIsModalOpen] = useState(true);
-  const navigate = useNavigate();
   const { id } = useParams();
-  const handleReplayMove = () => {
-    navigate(`/replay/${Number(id)}`);
-  };
 
   const audioRef = useRef<HTMLAudioElement | null>(null);
   const hlsRef = useRef<Hls | null>(null);
@@ -54,12 +50,6 @@ const Radio = () => {
       <div className={`${styles.container}`}>
         <div className="flex flex-row gap-7 items-center">
           <div className={`${styles.title}`}>인문/사회</div>{' '}
-          <button
-            className={`${styles.reply_button}`}
-            onClick={() => handleReplayMove()}
-          >
-            다시보기
-          </button>
         </div>
       </div>
 
