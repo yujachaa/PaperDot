9b140398fe5c55a7e8da226732f909f96cedf5ae
kwonja <kownja87@gmail.com>
2024-10-06 17:06:32 +0900

    feat: 라디오 북마크

--- Front/src/components/common/BookMark.tsx
+++ Front/src/components/common/BookMark.tsx
@@ -33,7 +33,7 @@ const BookMark = ({ className, paperId, bookmark }: BookMarkProps) => {
         className={`${className} cursor-pointer`}
         onClick={() => {
           clickBookmark(paperId);
-        }} //(미완) 북마크 추가/제거 api 호출하는 함수로 수정할 것
+        }}
       />
     </>
   );
