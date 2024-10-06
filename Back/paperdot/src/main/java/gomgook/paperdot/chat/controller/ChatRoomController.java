53c2931a9c0a147db14f572fbca759f5b7e68da1
김민경 <alsrud869@gmail.com>
2024-10-06 16:48:11 +0900

    fix: Fix unauthorized token error code to 401

---
+++ Back/paperdot/src/main/java/gomgook/paperdot/chat/controller/ChatRoomController.java
@@ -0,0 +1,9 @@
+package gomgook.paperdot.chat.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ChatRoomController {
+}
