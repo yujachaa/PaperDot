74a4729fa1c78c4dd456084037afc6347bb45dac
김민경 <alsrud869@gmail.com>
2024-10-07 20:49:57 +0900

    feat: add nickname of sender at chatDTO

--- Back/paperdot/src/main/java/gomgook/paperdot/chat/service/ChatService.java
+++ Back/paperdot/src/main/java/gomgook/paperdot/chat/service/ChatService.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import gomgook.paperdot.chat.dto.ChatMessageDto;
 import gomgook.paperdot.exception.CustomException;
 import gomgook.paperdot.exception.ExceptionResponse;
+import gomgook.paperdot.member.entity.Member;
+import gomgook.paperdot.member.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -17,10 +19,19 @@ import java.util.stream.Collectors;
 public class ChatService {
     @Autowired
     private StringRedisTemplate redisTemplate;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
     private ObjectMapper objectMapper = new ObjectMapper();
     // 메시지 저장 메서드
     public void saveMessage(int roomId, ChatMessageDto message) throws JsonProcessingException {
         String key = "chatroom:" + roomId;
+        Long memberId = message.getSenderId();
+
+        Member member = memberRepository.findById(memberId).orElse(null);
+        if(member!= null) message.setNickname(member.getNickname());
+        
         String jsonString = objectMapper.writeValueAsString(message);
         redisTemplate.opsForList().rightPush(key, jsonString);
     }
