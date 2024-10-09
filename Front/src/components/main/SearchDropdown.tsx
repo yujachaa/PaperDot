f9d002c44eafb5d749008b410a2fb6cce487f13c
이윤주 <yunj33@naver.com>
2024-10-09 13:32:24 +0900

    design: 드롭다운 css 수정

--- Front/src/components/main/SearchDropdown.tsx
+++ Front/src/components/main/SearchDropdown.tsx
@@ -13,51 +13,6 @@ interface SearchDropdownProps {
   }[];
 }
 
-// // 더미 데이터
-// const searchResult = [
-//   {
-//     id: 1,
-//     title:
-//       'React 사용법길어진논문제목길어진논문제목길어진논문제목길어진논문제목길어진논문제목길어진논문제목',
-//     author: 'John Doe',
-//     year: 2021,
-//     cnt: 5, // 북마크 개수
-//     bookmark: true, // 사용자가 북마크했는지 여부
-//   },
-//   {
-//     id: 2,
-//     title: 'TypeScript 기초길어진논문제목길어진논문제목길어진논문제목길어진논문제목',
-//     author: 'Jane Smith',
-//     year: 2020,
-//     cnt: 2,
-//     bookmark: false,
-//   },
-//   {
-//     id: 3,
-//     title: 'CSS Flexbox와 Grid길어진논문제목길어진논문제목',
-//     author: 'Alice Johnson',
-//     year: 2019,
-//     cnt: 7,
-//     bookmark: true,
-//   },
-//   {
-//     id: 4,
-//     title: 'JavaScript ES6의 새로운 기능',
-//     author: 'Bob Brown',
-//     year: 2018,
-//     cnt: 3,
-//     bookmark: false,
-//   },
-//   {
-//     id: 5,
-//     title: 'Node.js로 서버 구축하기',
-//     author: 'Charlie Green',
-//     year: 2017,
-//     cnt: 4,
-//     bookmark: true,
-//   },
-// ];
-
 // 하이라이트 처리 함수
 const highlightText = (text: string, highlight: string) => {
   if (!highlight.trim()) {
@@ -105,7 +60,7 @@ const SearchDropdown: React.FC<SearchDropdownProps> = ({ searchTerm, className, 
           </div>
         ))
       ) : (
-        <div className="text-light-text">검색 결과가 없습니다.</div>
+        <div className="text-light-text pl-5">검색 결과가 없습니다.</div>
       )}
     </div>
   );
