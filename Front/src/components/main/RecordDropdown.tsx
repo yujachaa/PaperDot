8f66ce6b922a3dc38f825a2c2f23e2911adb33a9
이윤주 <yunj333@gmail.com>
2024-10-09 13:32:24 +0900

    design: 드롭다운 css 수정

--- Front/src/components/main/RecordDropdown.tsx
+++ Front/src/components/main/RecordDropdown.tsx
@@ -54,7 +54,7 @@ const RecordDropdown: React.FC<DropDownProps> = ({ className, records, onDeleteR
           )}
         </ul>
       ) : (
-        <div className="text-light-text pl-11">이전 검색 기록이 없습니다.</div>
+        <div className="text-light-text pl-5">이전 검색 기록이 없습니다.</div>
       )}
     </div>
   );
