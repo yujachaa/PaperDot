d8303d198a2b4ffe6a846a7026dced9233b97152
이윤주 <yunj333@gmail.com>
2024-10-10 11:02:40 +0900

    fix: 라디오 썸네일 이미지 css 수정

--- Front/src/components/chat/ChatRoom.tsx
+++ Front/src/components/chat/ChatRoom.tsx
@@ -217,7 +217,7 @@ const ChatRoom = ({ className, paperId, roomId }: ChatRoomProps) => {
       <img
         src={notice}
         alt="공지사항"
-        className="absolute top-24 right-6 cursor-pointer z-30 object-cover"
+        className="absolute top-24 right-6 cursor-pointer z-30"
         onClick={() => {
           setIsNotice(true);
         }}
