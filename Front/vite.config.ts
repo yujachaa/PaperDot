a723a06eaac3c3548af46545d53c74f8ceb0be29
이윤주 <yunj33@naver.com>
2024-10-10 12:46:59 +0900

    feat: 콘솔 지우기

--- Front/vite.config.ts
+++ Front/vite.config.ts
@@ -3,10 +3,23 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-plugin-tsconfig-paths';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import removeConsole from 'vite-plugin-remove-console';
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    assetsInlineLimit: 20000, // 20KB로 설정
-  },
-  plugins: [react(), svgr(), tsconfigPaths(), nodePolyfills()],
+export default defineConfig(({ command }) => {
+  return {
+    build: {
+      assetsInlineLimit: 20000, // 20KB로 설정
+    },
+    plugins: [
+      react(),
+      svgr(),
+      tsconfigPaths(),
+      nodePolyfills(),
+      command === 'build' &&
+        removeConsole({
+          includes: ['log', 'warn', 'error'],
+        }),
+    ],
+  };
 });
