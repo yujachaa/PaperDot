ae434735a5a200cf2becb837c0e2831db4c80e02
kwonja <kownja87@gmail.com>
2024-10-10 10:55:12 +0900

    feat: AI 명령어 클릭이벤트 구현

--- Front/src/components/chat/SearhItem.tsx
+++ Front/src/components/chat/SearhItem.tsx
@@ -3,10 +3,18 @@ import styles from './SearhItem.module.scss';
 interface Props {
   command: string;
   focus: boolean;
+  onClick: (command: string) => void;
 }
 
-const SearchItem = ({ focus, command }: Props) => {
-  return <div className={`${styles.command} ${focus && `${styles.focus}`}`}>{command}</div>;
+const SearchItem = ({ focus, command, onClick }: Props) => {
+  return (
+    <div
+      onClick={() => onClick(command)}
+      className={`${styles.command} ${focus && `${styles.focus}`}`}
+    >
+      {command}
+    </div>
+  );
 };
 
 export default SearchItem;
